7b6eb170fbff0d1e249e879019962f74f8a5c581
Austin Bade <4bad20@protonmail.com>
2022-07-10 13:29:48 -0500

    Finished problem post fork
    Successfully uploaded, cloned and reuploaded on git hub

--- Challenge.two.py
+++ Challenge.two.py
@@ -1,1 +1,15 @@
-#Create a function that takes a base number and an exponent number and returns the calculation.
+# Create a function that takes a base number and an exponent number and returns the calculation.
+
+def calculate_exponent(num, exponent):
+    solution = num ** exponent
+    return solution
+
+
+number = int(input("Please enter a base number. "))
+
+expo = int(input("Please enter an exponent. "))
+
+answer = calculate_exponent(number, expo)
+
+print(f"The calculation of {number} to the {expo} power is {answer}. ")
+
