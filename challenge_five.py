9da477ed75b0597801957b3452ed778ac3caa572
Austin Bade <4bad20@protonmail.com>
2022-07-20 19:27:35 -0500

    Create challenge_five.py
    5th challenge involving data structurers

---
+++ challenge_five.py
@@ -0,0 +1,6 @@
+#Create a function that will remove any repeated character(s) in a word passed to the function. 
+# Not just consecutive characters, but characters repeating anywhere in the input.
+#No more than two words will be passed in the tests.
+#Input includes special characters and numbers.
+#unrepeated("hello") ➞ "helo"
+
