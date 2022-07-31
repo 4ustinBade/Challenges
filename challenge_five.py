3190cb33da95938b0169564f81ec5413eb915e8f
Austin Bade <4bad20@protonmail.com>
2022-07-31 14:10:32 -0500

    Update challenge_five.py
    made a program that takes a 2-d map of a skyline made out of 1 and 0 and determines how tall the tallest building in the skyline is

--- challenge_five.py
+++ challenge_five.py
@@ -1,7 +1,23 @@
-#Create a function that will remove any repeated character(s) in a word passed to the function. 
-# Not just consecutive characters, but characters repeating anywhere in the input.
-#No more than two words will be passed in the tests.
-#Input includes special characters and numbers.
-#unrepeated("hello") ➞ "helo"
+#A city skyline can be represented as a 2-D list with 1s representing buildings. 
+# In the example below, the height of the tallest building is 4 (second-most right column).
+#Create a function that takes a skyline (2-D list of 0's and 1's) and 
+# returns the height of the tallest skyscraper.
 
-12
+skyline = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [1, 0, 1, 0]]
+
+def find_skyline_height():
+    height = 0
+    for row in skyline:
+        print(row)
+        if 1 in row:
+            height += 1
+        else:
+            pass
+    return height 
+
+skyline_height = find_skyline_height()
+print(f"The height of the skyline is {skyline_height}.") 
