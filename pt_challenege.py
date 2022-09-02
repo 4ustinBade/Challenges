d1fa5a03cdda860039bace5e380524a588dc63dd
Austin Bade <4bad20@protonmail.com>
2022-09-02 11:25:50 -0500

    Update pt_challenege.py
    completed challenge reformatting date

--- pt_challenege.py
+++ pt_challenege.py
@@ -1,4 +1,35 @@
 #Create a function that converts a date formatted as MM/DD/YYYY to YYYYDDMM
 
 def format_date(date) : 
-    list[date]
+    list = []
+    final_string = ''
+    # turn string into a list
+    for x in range(len(date)):
+        list.append(date[x])
+    # Remove '/' from list
+    for x in range(len(list)-2):
+        if list[x] ==1 '/':
+            list.remove(list[x])
+        else:
+            pass
+     # Reformat date to front
+    for z in range(4):
+        list.append(list[0])
+        list.pop(0)
+    # Final format
+    for i in range(len(list)):
+        if i <= 3 or i >= 6:
+            pass
+        else:
+            list.append(list[4])
+            list.pop(4)
+    #turn list into string
+    for x in range(len(list)):
+        final_string += list[x]
+    
+    return final_string
+
+
+user_input = input("Type date in format mm/dd/yyyy.\n")
+answer = format_date(user_input)
+print(answer)
